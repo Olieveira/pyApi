5e5514f331c1a0263f0c4224c5e845cdae3a94a6
Nathan Oliveira <nathanmello2@outlook.com>
2025-04-27 20:53:35 -0300

    Correção na importação

--- test/test.py
+++ test/test.py
@@ -15,7 +15,7 @@ from src.main import app
 @pytest.mark.asyncio
 async def test_get_moedas():
     """Test the /moedas endpoint."""
-    async with AsyncClient(base_url="http://test") as client:
+    async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.get("/moedas")
         assert response.status_code == 20
         assert isinstance(response.json(), list)
@@ -23,7 +23,7 @@ async def test_get_moedas():
 @pytest.mark.asyncio
 async def test_moedas_by_id():
     """Test the /moedas/{id} endpoint."""
-    async with AsyncClient(base_url="http://test") as client:
+    async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.get("/moedas/bitcoin")
         assert response.status_code == 200
         response_json = response.json()
@@ -32,7 +32,7 @@ async def test_moedas_by_id():
 @pytest.mark.asyncio
 async def test_get_nfts():
     """Test the /nfts endpoint."""
-    async with AsyncClient(base_url="http://test") as client:
+    async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.get("/nfts")
         assert response.status_code == 200
         assert isinstance(response.json(), list)
@@ -40,7 +40,7 @@ async def test_get_nfts():
 @pytest.mark.asyncio
 async def test_nfts_by_id():
     """Test the /nfts/{id} endpoint."""
-    async with AsyncClient(base_url="http://test") as client:
+    async with AsyncClient(app=app, base_url="http://test") as client:
         response = await client.get("/nfts/hashmasks")
         assert response.status_code == 200
         response_json = response.json()
